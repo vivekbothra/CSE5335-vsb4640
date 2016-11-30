2bb801b6236542bc15d0a770ca9d5b0fdb68125a
vivekbothra <vivekbothra17@gmail.com>
2016-11-29 21:20:07 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,129 +1,140 @@
-<?php
+  <?php
 
 
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "webdata";
+  // $servername = "localhost";
+  // $username = "root";
+  // $password = "";
+  // $dbname = "webdata";
 
-// Create connection
-// $conn = mysqli_connect($servername, $username, $password, $dbname);
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+  // // Create connection
+  // $conn = mysqli_connect($servername, $username, $password, $dbname);
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
 
-$conn = new mysqli($server, $username, $password, $db);
+  $conn = new mysqli($server, $username, $password, $db);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-// $sql = "DROP table apidata";
-// $sql = "CREATE TABLE apidata (
-// id INT(150) NOT NULL  PRIMARY KEY,
-// userId INT(150) UNSIGNED, 
-// title VARCHAR(1000) NOT NULL,
-// body VARCHAR(1000)
+  // Check connection
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+  // $sql = "DROP table apidata";
+  // $sql = "CREATE TABLE apidata (
+  // id INT(150) NOT NULL  PRIMARY KEY,
+  // userId INT(150) UNSIGNED, 
+  // title VARCHAR(1000) NOT NULL,
+  // body VARCHAR(1000)
 
-// )";
+  // )";
 
-// if (mysqli_query($conn, $sql)) {
-//     echo "Table apidata created successfully";
-// } else {
-//     echo "Error creating table: " . mysqli_error($conn);
-// }
+  // if (mysqli_query($conn, $sql)) {
+  //     echo "Table apidata created successfully";
+  // } else {
+  //     echo "Error creating table: " . mysqli_error($conn);
+  // }
 
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
-$ch=curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://jsonplaceholder.typicode.com/posts');
+  // error_reporting(E_ALL);
+  // ini_set('display_errors', 1);
+  $ch=curl_init();
+  curl_setopt($ch, CURLOPT_URL, 'https://jsonplaceholder.typicode.com/posts');
 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
 
-$output = curl_exec($ch);
+  $output = curl_exec($ch);
 
-if($output == FALSE){
-	echo "curl error" . curl_error($ch);
+  if($output == FALSE){
+    echo "curl error" . curl_error($ch);
 
-}
-curl_close($ch);
-$out=json_decode($output);
-  // print_r($out);
+  }
+  curl_close($ch);
+  $out=json_decode($output);
+    // print_r($out);
 
-foreach ($out as $key => $value) {
-	# code...
-	$array = json_decode(json_encode($value), true);
-	 // print_r($array);
-	$keys = array_map(null, array_keys($array));
-	 // print_r($keys);
-	$newarray = array_map(null, $array);
-	// print_r($newarray);
+  foreach ($out as $key => $value) {
+    # code...
+    $array = json_decode(json_encode($value), true);
+     // print_r($array);
+    $keys = array_map(null, array_keys($array));
+     // print_r($keys);
+    $newarray = array_map(null, $array);
+    // print_r($newarray);
 
-// 	  $sql = "INSERT INTO apidata(`".implode('`, `', $keys)."`) VALUES('".implode("', '", $newarray)."')";
+  //    $sql = "INSERT INTO apidata(`".implode('`, `', $keys)."`) VALUES('".implode("', '", $newarray)."')";
 
-// 	if (mysqli_query($conn, $sql)) {
-//    echo "inserted successfully";
-// } else {
-//    echo "Error inserting: " . mysqli_error($conn);
-// }
+  //  if (mysqli_query($conn, $sql)) {
+  //    echo "inserted successfully";
+  // } else {
+  //    echo "Error inserting: " . mysqli_error($conn);
+  // }
 
-}
-
-
-
-mysqli_close($conn);
+  }
 
 
-?>
-<body><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script type="text/javascript">
 
-$(document).ready(function(){
+  mysqli_close($conn);
 
-	for (var i = 1; i <=100; i++) {
 
-     //   $.get( 
-                 //    "getrecords1.php",
-                 //    { id: i },
-                 //    function(data) {
-                 //       $('#res3').append(data);
-                 //       setTimeout(5000);
-                 //    }
-                 // );
-                 console.log("inside ajax call");
-                 
-                 $.ajax({
+  ?>
+  <body><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script type="text/javascript">
 
-                   type: 'GET',
-                   url: 'getrecords.php',
-                   data: {id: i},
-                   success: function(data) {
+  $(document).ready(function(){
+    
+    function doSetTimeout(i) {
+
                    
-                     $('#res3').append(data);
-                     $('#res3').animate({
-        left: '250px',
-        opacity: '0.7'
-        
-    });
+
+                                       $.ajax({
+
+                                   type: 'GET',
+                                    url: 'getrecords.php',
+                                   data: {id: i},
+
+                                    success: function(data) {
                      
-                      setTimeout(500);
-                   }
+                                  $('#res3').append(data);
+
+                                  $('#res3').animate({
+                                        left: '250px',
+                                        opacity: 'toggle',
+                                        
+          
+                                          });
+                                  
+                       
+                                        
+                                               }
 
 
-                 });
+                                          });
+                                       
+                                       if(i<100){
+                                       
 
+                                       setTimeout(function(){i++;
+                                        console.log(i);
+                                        doSetTimeout(i);}, 500);
+                                     }
+                        
+            
 
-             }
-                
-                });
+                          }
+                          doSetTimeout(1);
 
-  </script>
+                         
+          
 
-<div id="res3"></div>
-</body>
+                  
+                  });
+
+    </script>
+
+  <div id="res3"></div>
+  </body>
+   
  
